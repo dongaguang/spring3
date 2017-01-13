8c2007ebb56ed37188de324b4411872f81874eb7
董书广 <18756003663@139.com>
2017-01-13 09:15:20 +0800

    测试

---
+++ test/bbt/user/service/TestBbtUserService.java
@@ -0,0 +1,19 @@
+package bbt.user.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * JUNIT4测试用户登陆的业务层
+ * @author Administrator
+ *
+ */
+public class TestBbtUserService {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
